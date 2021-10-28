e1f8acb6220a5d5e43e50e3777fe4e21dcd46159
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-10-29 01:30:40 +0400

    Update TransactionManager

--- app/src/main/java/com/example/superbank/manager/TransactionManager.java
+++ app/src/main/java/com/example/superbank/manager/TransactionManager.java
@@ -6,6 +6,7 @@ import androidx.annotation.RequiresApi;
 
 import com.example.superbank.entity.BankAccount;
 import com.example.superbank.entity.Transaction;
+import com.example.superbank.enums.TransactionCategory;
 import com.example.superbank.payload.request.transaction.requestDto.TransactionRequestDto;
 import com.example.superbank.payload.request.transaction.requestDto.TransactionRequestDtoBuilder;
 import com.example.superbank.payload.response.TransactionResponseDto;
@@ -16,11 +17,10 @@ import java.util.ArrayList;
 
 /**
  * {@link TransactionManager} is a link between UI and {@link TransactionService}.
- *
+ * <p>
  * While {@link TransactionService} cares only about saving the results of transactions into the database
  * and account's transaction history, {@link TransactionManager} validates transactions and makes rollbacks if
  * it is requested.
- *
  */
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class TransactionManager {
@@ -46,22 +46,27 @@ public class TransactionManager {
 
         ArrayList<Integer> errorCodes = new ArrayList<>();
 
-        if(!bankAccountService.existsById(senderId)){
+        if (!bankAccountService.existsById(senderId)) {
             errorCodes.add(1);
         }
 
-        if(!bankAccountService.existsById(receiverId)){
+        if (!bankAccountService.existsById(receiverId)) {
             errorCodes.add(2);
         }
 
-        BankAccount sender = bankAccountService.getSecured(receiverId);
-        BankAccount receiver = bankAccountService.getSecured(senderId);
+        if (senderId.equals(receiverId)) {
+            errorCodes.add(5);
+        }
+
+        if (errorCodes.size() != 0) return formErrorDto(errorCodes);
+
+        BankAccount sender = bankAccountService.getSecured(senderId);
 
         if (sender.getAvailableMoney() - amountOfMoney < 0) {
             errorCodes.add(3);
         }
 
-        if(errorCodes.size() == 0){
+        if (errorCodes.size() == 0) {
             return transactionService.add(requestDto);
         }
 
@@ -70,22 +75,48 @@ public class TransactionManager {
     }
 
     /**
-     *
      * @param transactionId Id of the transaction which is needed to be rollbacked
      * @return New rollback transaction
-     *
+     * <p>
      * Forms a new transaction based on the initial transaction (works just like giving money back to the sender)
      * and saves it, leaving the initial one untouched in the database
+     * <p>
+     * Is not safe!
+     * <p>
+     * (Is not ready yet)
      */
 
     public TransactionResponseDto rollbackTransaction(Long transactionId) {
         Transaction transaction = transactionService.getSecured(transactionId);
 
-
         return new TransactionResponseDto();
     }
 
-    public TransactionResponseDto formErrorDto(ArrayList<Integer> errorCodes){
+    //Refactoring will be soon
+
+    public TransactionResponseDto doCashTransaction(Long receiverId, Double amountOfMoney, TransactionCategory category) {
+        ArrayList<Integer> errorCodes = new ArrayList<>();
+
+        if (!bankAccountService.existsById(receiverId))
+            errorCodes.add(1);
+
+
+        if (category != TransactionCategory.CASH_WITHDRAW && category != TransactionCategory.CASH_SUPPLY)
+            errorCodes.add(4);
+
+        if (errorCodes.size() != 0) {
+            return formErrorDto(errorCodes);
+        }
+
+        TransactionRequestDtoBuilder builder = prepareTransaction();
+        builder.setAmountOfMoney(amountOfMoney);
+        builder.setCategory(category);
+        builder.setReceiver(receiverId);
+
+        return transactionService.addSingleSidedTransaction(builder.build());
+    }
+
+    public TransactionResponseDto formErrorDto(ArrayList<Integer> errorCodes) {
         TransactionResponseDto responseDto = new TransactionResponseDto();
         responseDto.setError(true);
         responseDto.setErrorCodes(errorCodes);
