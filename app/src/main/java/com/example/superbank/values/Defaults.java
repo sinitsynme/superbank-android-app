1811e74d6ba8eeb608aac9f4ef3cabe005dfb7dd
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2021-12-14 02:43:58 +0400

    Update Defaults.java

--- app/src/main/java/com/example/superbank/values/Defaults.java
+++ app/src/main/java/com/example/superbank/values/Defaults.java
@@ -3,8 +3,8 @@ package com.example.superbank.values;
 public class Defaults {
 
     public static final String SERVER_URL = "https://eu-api.backendless.com";
-    public static final String APPLICATION_ID = "AC9A014D-A301-0524-FFEC-754540640100";
-    public static final String API_KEY = "CA9E1115-ED9C-41DB-A002-6CD519345C16";
+    public static final String APPLICATION_ID = "YOUR_APP_ID";
+    public static final String API_KEY = "YOUR_API_KEY";
 
 
 }
